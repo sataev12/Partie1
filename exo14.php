9939b2091de2025c5da7389a91bef829f7f6a981
John Doe11 <sataevmagomed.eu@gmail.com>
2024-01-25 22:32:23 +0100

    Ajout d'un exo14 PHP partie1

---
+++ exo14.php
@@ -0,0 +1,12 @@
+<h1>Exercice 14</h1>
+
+<p>Calculer l'âge exact d'une personne à partir de sa date de naissance (en années, mois, jours).</p>
+
+<?php
+
+$dateNaissance = date_create("17-01-1985"); //La date de naissance
+$aujourdui = date_create(); //Recuperer la date du jour
+
+$interval = date_diff($dateNaissance, $aujourdui); //Interval entre les 2 dates
+
+echo $interval->format("%y ans-%m mois-%d jours "); //Affichage
